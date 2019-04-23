e76b960a7abaa273adb9197ce232d83623050216
N4J1B <48822699+N4J1B@users.noreply.github.com>
2019-04-23 18:50:48 +0700

    Add files via upload

---
+++ Kw.sh
@@ -0,0 +1,142 @@
+#system/bin/sh
+
+clear
+toilet -f big -F gay "Tools Hack" | lolcat
+toilet -f big -F gay "                                 PLN"
+echo "======================================================="|lolcat
+echo "# Coded   :QWERTY20/N4J1B
+# Github  :https://github.com/N4J1B
+# Contact :+62 898-3306-096
+# IG      :@m.ainunnjib"|lolcat
+echo "======================================================="|lolcat
+echo "Pilih District/tempat untuk dimatikan:"|lolcat
+echo "[=============================]>"|lolcat
+echo "1.> Sidoarjo"|lolcat
+echo "[=============================]>"|lolcat
+echo "2.> Krian"|lolcat
+echo "[=============================]>"|lolcat
+echo "3.> Sukodono"|lolcat
+echo "[=============================]>"|lolcat
+echo "4.> Balong Bendo"|lolcat
+echo "[=============================]>"|lolcat
+echo "5.> Buduran"|lolcat
+echo "[=============================]>"|lolcat
+echo "6.> Candi"|lolcat
+echo "[=============================]>"|lolcat
+echo "7.> Gedangan"|lolcat
+echo "[=============================]>"|lolcat
+echo "8.> Jabon"|lolcat
+echo "[=============================]>"|lolcat
+echo "9.> Krembung"|lolcat
+echo "[=============================]>"|lolcat
+echo "10.> Prambon"|lolcat
+echo "[=============================]>"|lolcat
+echo "11.> Porong"|lolcat
+echo "[=============================]>"|lolcat
+echo "12.> Sedati"|lolcat
+echo "[=============================]>"|lolcat
+echo "13.> Taman"|lolcat
+echo "[=============================]>"|lolcat
+echo "14.> Tanggulangin"|lolcat
+echo "[=============================]>"|lolcat
+echo "15.> Tarik"|lolcat
+echo "[=============================]>"|lolcat
+echo  "16.> Tulangan"|lolcat
+echo "[=============================]>"|lolcat
+echo  "17.> Waru"|lolcat
+echo "[=============================]>"|lolcat
+echo "18.> Wonoayu"|lolcat
+echo "[=============================]>"|lolcat
+echo "Input nomor:"|lolcat
+read nomor
+
+if [ $nomor = 1 ] || [ $nomor = 01 ]
+then
+sh Loading.sh
+fi
+
+
+if [ $nomor = 2 ] || [ $nomor = 02 ]
+then
+sh Loading.sh
+fi
+
+if [ $nomor = 3 ] || [ $nomor = 03 ]
+then
+sh Loading.sh
+fi
+
+if [ $nomor = 4 ] || [ $nomor = 04 ]
+then
+sh Loading.sh
+fi
+
+if [ $nomor = 5 ] || [ $nomor = 05 ]
+then
+sh Loading.sh
+fi
+
+if [ $nomor = 6 ] || [ $nomor = 06 ]
+then
+sh Loading.sh
+fi
+
+if [ $nomor = 7 ] || [ $nomor = 07 ]
+then
+sh Loading.sh
+fi
+
+if [ $nomor = 8 ] || [ $nomor = 08 ]
+then
+sh Loading.sh
+fi
+
+if [ $nomor = 9 ] || [ $nomor = 09 ]
+then
+sh Loading.sh
+fi
+
+if [ $nomor = 10 ] || [ $nomor = 10 ]
+then
+sh Loading.sh
+fi
+
+if [ $nomor = 11 ]
+then
+sh Loading.sh
+fi
+
+if [ $nomor = 12 ]
+then
+sh Loading.sh
+fi
+
+if [ $nomor = 13 ]
+then
+sh Loading.sh
+fi
+
+if [ $nomor = 14 ]
+then
+sh Loading.sh
+fi
+
+if [ $nomor = 15 ]
+then
+sh Loading.sh
+fi
+
+if [ $nomor = 16 ]
+then
+sh Loading.sh
+fi
+
+if [ $nomor = 17 ]
+then
+sh Loading.sh
+fi
+
+if [ $nomor = 18 ]
+then
+sh Loading.sh
+fi
