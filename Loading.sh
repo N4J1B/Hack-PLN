1521e7b17c76326ccdedbf035b437644bc097abb
N4J1B <48822699+N4J1B@users.noreply.github.com>
2019-04-23 18:51:59 +0700

    Add files via upload

---
+++ Loading.sh
@@ -0,0 +1,13 @@
+clear
+echo "Menunggu Server terhubung ..."|lolcat
+echo "Menjalankan Perintah..."|lolcat
+
+echo "3"|lolcat
+
+echo "2"|lolcat
+
+echo "1"|lolcat
+
+echo "Mematikan Listrik area yang diinginkan..."|lolcat
+echo "Listrik dimatikan...[100%]"|lolcat
+figlet "Berhasil dimatikan"|lolcat
